3ce0c447b7233f12e37d84f7ce201ad7cefa916d
Wilmer Morales <moralesw.1981@gmail.com>
2024-08-28 11:41:06 -0500

    no changes

--- scripts/PurchaseBtn.js
+++ scripts/PurchaseBtn.js
@@ -1,14 +1,13 @@
-import { purchaseMineral } from './TransientState.js';
-
+import { purchaseMineral } from "./TransientState.js";
 
 const handleOrderSubmissionClick = (clickEvent) => {
   if (clickEvent.target.id == "purchaseBtn") {
     purchaseMineral();
   }
-}
+};
 
 export const purchaseBtn = () => {
-  document.addEventListener("click", handleOrderSubmissionClick)
+  document.addEventListener("click", handleOrderSubmissionClick);
   return `
     <div class=btn-container>
     <h2>Space Cart</h2>
